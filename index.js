cc7ec0ec70263390153820ff4918decdddffc278
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2021-11-14 23:04:11 +0100

    part3c done

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const Note = require("./models/note");
 
-function requestLogger(req, res, next) {
+
+function requestLogger(req, _, next) {
   console.log("Method:", req.method);
   console.log("Path:", req.path);
   console.log("Bdy:", req.body);
@@ -19,33 +23,6 @@ function unknownEndpoint(req, res) {
 }
 
 
-function generateId(notes) {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(note => note.id)) : 0;
-  return maxId +1;
-}
-
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-];
-
 app.use(express.static("build"));
 
 app.use(cors());
@@ -55,52 +32,41 @@ app.use(express.json());
 app.use(requestLogger);
 
 app.get("/api/notes", (_, res) => {
-  res.json(notes);
+  Note.find({}).then(notes => res.json(notes));
 });
 
 
 app.get("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const note = notes.find(note => note.id === id);
-
-  if (note) {
-    res.json(note);
-  } else {
-    res.status(404).end();
-  }
+  Note.findById(req.params.id).then(note => res.json(note));
 });
 
 
 app.delete("/api/notes/:id", (req, res) => {
-  const id = Number(req.params.id);
-  notes = notes.filter(note => note.id !== id);
-
-  res.status(204).end();
 });
 
 
 app.post("/api/notes", (req, res) => {
   if (!req.body.content) {
     return res.status(400).json({
-      error: "content field missing"
+      error: "content missing"
     });
   }
 
-  const note = {
-    id: generateId(notes),
+  const note = new Note({
     content: req.body.content,
-    important: req.body.content || false,
-    date: new Date().toISOString()
-  } 
+    important: req.body.important || false,
+    date: new Date()
+  });
 
-  notes = notes.concat(note);
-  res.json(note);
+  note.save().then(savedNote => {
+    res.json(savedNote);
+  });
 });
 
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, ()=> {
   console.log(`Server running on port ${PORT}`);
 });
